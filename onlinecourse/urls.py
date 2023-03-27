8bf5767750d404e288d1d50e0da2be2b2637d234
Andrew <andrew@andrewbritton.com>
2023-03-26 23:18:20 -0700

    Added URLs for 'submit_exam' and 'exam_result'

--- onlinecourse/urls.py
+++ onlinecourse/urls.py
@@ -18,7 +18,9 @@ urlpatterns = [
     path('<int:course_id>/enroll/', views.enroll, name='enroll'),
 
     # <HINT> Create a route for submit view
+    path('<int:course_id>/submit_exam/', views.submit_exam, name='submit_exam'),
 
     # <HINT> Create a route for show_exam_result view
+    path('<int:submission_id>/exam_result/', views.show_exam_result, name='exam_result'),
 
  ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
